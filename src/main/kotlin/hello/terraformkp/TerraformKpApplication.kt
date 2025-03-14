dbc08eb4b71a44143ea49e35edd7e5e52b655d4a
jurio5 <sia5492@naver.com>
2025-03-14 11:20:23 +0900

    first

---
+++ src/main/kotlin/hello/terraformkp/TerraformKpApplication.kt
@@ -0,0 +1,11 @@
+package hello.terraformkp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TerraformKpApplication
+
+fun main(args: Array<String>) {
+    runApplication<TerraformKpApplication>(*args)
+}
---
+++ src/test/kotlin/hello/terraformkp/TerraformKpApplicationTests.kt
@@ -0,0 +1,13 @@
+package hello.terraformkp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TerraformKpApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
